73f403f8f03b8bb65e1d3af5c3721ce93ed007d9
Shun-T-Engineer <shun.0520.1988@gmail.com>
2025-06-15 19:37:24 +0900

    コントローラー作成

--- src/main/java/com/example/demo/controller/ParamController.java
+++ src/main/java/com/example/demo/controller/ParamController.java
@@ -1,8 +1,21 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ParamController {
 
+	@GetMapping("/input")
+	public String input() {
+		return "input";
+	}
+	
+	@GetMapping("/post-param")
+	public String postParam(@RequestParam String greeting, Model model) {
+		model.addAttribute("mGreeting", greeting);
+		return "display";
+	}
 }
